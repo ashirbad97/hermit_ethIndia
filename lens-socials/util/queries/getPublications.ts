e318e8ff60beeab28a57a3c68694c6479930e239
Ananth <ananthmadhavan6@gmail.com>
2022-12-03 11:52:15 +0530

    add publicationsByProfileId API

---
+++ lens-socials/util/queries/getPublications.ts
@@ -0,0 +1,42 @@
+export const getPublications = `
+  query Publications($profileId: ProfileId!, $limit: LimitScalar) {
+    publications(request: {
+      profileId: $profileId,
+      publicationTypes: [POST]
+      limit: $limit
+    }) {
+      items {
+        __typename 
+        ... on Post {
+          ...PostFields
+        }
+      }
+    }
+  }
+  fragment PostFields on Post {
+    id
+    metadata {
+      ...MetadataOutputFields
+    }
+    createdAt
+  }
+  fragment MetadataOutputFields on MetadataOutput {
+    name
+    description
+    content
+    media {
+      original {
+        ...MediaFields
+      }
+    }
+    attributes {
+      displayType
+      traitType
+      value
+    }
+  }
+  fragment MediaFields on Media {
+    url
+    mimeType
+  }
+`;
