fd60d0d25d53173f8c827d685503010f700a35c4
Ananth <ananthmadhavan6@gmail.com>
2022-12-03 17:51:01 +0530

    fix next js api

--- lens-socials/pages/api/fetchExploreProfiles.ts
+++ lens-socials/pages/api/fetchExploreProfiles.ts
@@ -2,8 +2,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "urql";
 import { exploreProfiles } from "../../util/queries/getExploreProfiles";
-type Data = {
+
+type Profile = {
+  id: string;
   name: string;
+  bio: string;
+  handle: string;
+  picture: {
+    original: {
+      url: string;
+    };
+  };
+  coverPicture: {
+    original: {
+      url: string;
+    };
+  };
+  stats: {
+    totalFollowers: number;
+    totalFollowing: number;
+    totalPosts: number;
+    totalComments: number;
+    totalMirrors: number;
+    totalPublications: number;
+    totalCollects: number;
+  };
+};
+type Data = {
+  profiles: Profile[];
 };
 
 const APIURL = `https://api-mumbai.lens.dev/`; //Mumbai Testnet API
@@ -16,40 +42,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
-  let profilesList: Array<{
-    id: string;
-    name: string;
-    bio:string; 
-    handle: string;
-    picture: {
-      original: {
-        url: string;
-      }
-    };
-    coverPicture: {
-      original: {
-        url: string;
-      }
-    };
-    stats: {
-      totalFollowers: number;
-      totalFollowing: number;
-      totalPosts: number;
-      totalComments: number;
-      totalMirrors: number;
-      totalPublications: number;
-      totalCollects: number;
-    };
-  }>
+  let profilesList: Profile[];
   try {
+    let sort_criteria = req.query.sortCriteria;
     const response = await client
-      .query(exploreProfiles, { req.query.sortCriteria })
+      .query(exploreProfiles, { sortCriteria: sort_criteria })
       .toPromise();
-    const profilesList = response.data.exploreProfiles.items;
-    return profilesList;
+    profilesList = response.data.exploreProfiles.items;
   } catch (error) {
     console.log(`fetchExploreProfiles failed due to ` + error);
   }
-  res.status(200).json( profilesList );
+  res.status(200).json({ profiles: profilesList });
 }
