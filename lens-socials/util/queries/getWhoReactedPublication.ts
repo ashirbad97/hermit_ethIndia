f03ba3f3c1d5adee6587df11ccd682e789451498
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-03 13:04:36 +0530

    added whoReactedPublication API

---
+++ lens-socials/util/queries/getWhoReactedPublication.ts
@@ -0,0 +1,127 @@
+export const whoReactedPublication = `query WhoReactedPublication ($publicationId:InternalPublicationId!){
+    whoReactedPublication(request: { publicationId: $publicationId }) {
+      items {
+        reactionId
+        reaction
+        reactionAt
+        profile {
+          ...ProfileFields
+        }
+      }
+      pageInfo {
+        prev
+        next
+        totalCount
+      }
+    }
+  }
+  
+  fragment MediaFields on Media {
+    url
+    width
+    height
+    mimeType
+  }
+  
+  
+  fragment ProfileFields on Profile {
+    id
+    name
+    bio
+    attributes {
+      displayType
+      traitType
+      key
+      value
+    }
+    isFollowedByMe
+    isFollowing(who: null)
+    followNftAddress
+    metadata
+    isDefault
+    handle
+    picture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        verified
+      }
+      ... on MediaSet {
+        original {
+          ...MediaFields
+        }
+        small {
+          ...MediaFields
+        }
+        medium {
+          ...MediaFields
+        }
+      }
+    }
+    coverPicture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        verified
+      }
+      ... on MediaSet {
+        original {
+          ...MediaFields
+        }
+        small {
+          ...MediaFields
+        }
+        medium {
+          ...MediaFields
+        }
+      }
+    }
+    ownedBy
+    dispatcher {
+      address
+    }
+    stats {
+      totalFollowers
+      totalFollowing
+      totalPosts
+      totalComments
+      totalMirrors
+      totalPublications
+      totalCollects
+    }
+    followModule {
+      ...FollowModuleFields
+    }
+  }
+  
+  fragment FollowModuleFields on FollowModule {
+    ... on FeeFollowModuleSettings {
+      type
+      amount {
+        asset {
+          name
+          symbol
+          decimals
+          address
+        }
+        value
+      }
+      recipient
+    }
+    ... on ProfileFollowModuleSettings {
+      type
+      contractAddress
+    }
+    ... on RevertFollowModuleSettings {
+      type
+      contractAddress
+    }
+    ... on UnknownFollowModuleSettings {
+      type
+      contractAddress
+      followModuleReturnData
+    }
+  }
+  `;
