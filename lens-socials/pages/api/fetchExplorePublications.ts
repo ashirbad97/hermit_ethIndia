5339c489239b365c89502a5d8b289b507dde2b18
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-03 12:34:38 +0530

    fixed file name for fetchExplorePublications API

---
+++ lens-socials/pages/api/fetchExplorePublications.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "urql";
+import { explorePublications } from "../../util/queries/getExplorePublications";
+type Data = {
+  name: string;
+};
+
+const APIURL = `https://api-mumbai.lens.dev/`; //Mumbai Testnet API
+
+export const client = createClient({
+  url: APIURL,
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  let userPublicationList = {}
+  try {
+    const response = await client
+      .query(explorePublications, { req.query.profileId, req.query.limit })
+      .toPromise();
+    const userPublicationList = response.data?.publications?.items;
+    return userPublicationList;
+  } catch (error) {
+    console.log(`fetchExplorePublications failed due to ` + error);
+  }
+  res.status(200).json( userPublicationList );
+}
