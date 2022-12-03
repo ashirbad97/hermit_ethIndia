3c21548b87781fda3724ea285387468d4ee7bc37
Ananth <ananthmadhavan6@gmail.com>
2022-12-04 01:55:17 +0530

    more cosmetic changes

--- lens-socials/components/PostField.tsx
+++ lens-socials/components/PostField.tsx
@@ -4,22 +4,28 @@ import Image from "next/image";
 const PostField: FC = () => {
   return (
     <>
-      <div className="container bg-white flex flex-col">
-        <div>
+      <div className="container bg-white flex flex-col rounded-lg">
+        <div className="p-4 flex gap-6">
           <Image
             src="/img_avatar.png"
-            width={64}
-            height={64}
+            width={128}
+            height={128}
             alt="avatar"
-            className="h-8 rounded-full w-8"
+            className="h-16 rounded-full w-16 "
           />
           <input
             placeholder="Sup?"
-            className="placeholder:text-gray-500 text-gray-500 text-sm"
+            className="placeholder:text-gray-300 
+            text-gray-600
+            outline-none text-3xl font-myriad"
           />
         </div>
-        <div className="mt-2">
-          <button className="cursor-pointer text-white p-3 rounded-xl font-bold bg-emerald-500 btn">
+        <div className="mt-2 flex flex-row-reverse mr-8 mb-4">
+          <button
+            className="cursor-pointer text-white p-4
+           rounded-xl font-bold bg-emerald-500 
+           btn text-xl"
+          >
             Post
           </button>
         </div>
