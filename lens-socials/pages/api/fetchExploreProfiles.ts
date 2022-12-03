2ec867dc9e334268d43c9deb9150ac1a9501044b
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-03 12:31:49 +0530

    added explorePublications API

--- lens-socials/pages/api/fetchExploreProfiles.ts
+++ lens-socials/pages/api/fetchExploreProfiles.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "urql";
-import { getPublications } from "../../util/queries/getPublicationsByProfileId";
+import { explorePublications } from "../../util/queries/getExplorePublications";
 type Data = {
   name: string;
 };
@@ -20,12 +20,12 @@ export default async function handler(
   let userPublicationList = {}
   try {
     const response = await client
-      .query(getPublications, { req.query.profileId, req.query.limit })
+      .query(explorePublications, { req.query.profileId, req.query.limit })
       .toPromise();
     const userPublicationList = response.data?.publications?.items;
     return userPublicationList;
   } catch (error) {
-    console.log(`fetchPublicationsProfileId failed due to ` + error);
+    console.log(`fetchExplorePublications failed due to ` + error);
   }
   res.status(200).json( userPublicationList );
 }
