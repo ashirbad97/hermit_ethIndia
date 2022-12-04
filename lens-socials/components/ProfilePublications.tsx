0f70f4a64de87949f2150c347aba48b910047db1
Ananth <ananthmadhavan6@gmail.com>
2022-12-04 06:58:38 +0530

    more ui changes to profile page

--- lens-socials/components/ProfilePublications.tsx
+++ lens-socials/components/ProfilePublications.tsx
@@ -4,23 +4,20 @@ import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const ProfilePublications: FC<{ profileId: string }> = ({ profileId }) => {
-    console.log(profileId);
   const { data, error } = useSWR(
     `/api/fetchPublicationsByProfileId?profileId=${profileId}&limit=10`,
     fetcher
   );
 
-//   console.log(data);
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
   return (
     <>
-      <div className="flex flex-col font-myriad mb-8">
+      <div className="flex flex-col font-myriad mb-8 container mx-auto">
         {data.userPublication?.map((publication: PublicationType) => (
           <div className="rounded-lg bg-white mt-4 p-6 flex flex-col">
             <div className="flex gap-4">
-              
               <div className=" justify-center col-span-4 flex flex-col">
                 <div className="text-emerald-800 font-semibold text-lg">
                   {publication.metadata.name}
@@ -41,5 +38,3 @@ const ProfilePublications: FC<{ profileId: string }> = ({ profileId }) => {
 };
 
 export default ProfilePublications;
-
-
--- lens-socials/pages/profile.tsx
+++ lens-socials/pages/profile.tsx
@@ -13,22 +13,19 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const UserProfile: FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
-  
+
   // fetch and set profile handle here
   const viewingpProfileHandle = "yoginth.test";
-
-
 
   //your profile handle will be passed from parent
   const yourProfileId = "ashirbad97.test";
 
-  const { data , error } = useSWR(
+  const { data, error } = useSWR(
     `/api/fetchProfileDetailsByHandle?handles=${viewingpProfileHandle}`,
     fetcher
   );
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  
 
   // fetching the profileID from fetched data to get the mutual followers
   // const viewingProfileId: string = data?.profileDetails[0].id;
@@ -37,99 +34,118 @@ const UserProfile: FC = () => {
 
   let profilePic: string = "";
   if (data?.profileDetails[0]?.picture?.original?.url != undefined) {
-    profilePic = (data.profileDetails[0].picture?.original?.url).includes("ipfs://")
-      ? "https://ipfs.io/" + data.profileDetails[0].picture?.original?.url.replace(":/", "")
+    profilePic = (data.profileDetails[0].picture?.original?.url).includes(
+      "ipfs://"
+    )
+      ? "https://ipfs.io/" +
+        data.profileDetails[0].picture?.original?.url.replace(":/", "")
       : data.profileDetails[0].picture?.original?.url;
   }
 
   let coverPic: string = "";
   if (data?.profileDetails[0]?.coverPicture?.original?.url != undefined) {
-    coverPic = (data.profileDetails[0].coverPicture?.original?.url).includes("ipfs://")
-      ? "https://ipfs.io/" + data.profileDetails[0].coverPicture?.original?.url.replace(":/", "")
+    coverPic = (data.profileDetails[0].coverPicture?.original?.url).includes(
+      "ipfs://"
+    )
+      ? "https://ipfs.io/" +
+        data.profileDetails[0].coverPicture?.original?.url.replace(":/", "")
       : data.profileDetails[0].coverPicture?.original?.url;
   } else {
     coverPic = "https://picsum.photos/1600/450";
   }
-console.log(coverPic);
-  
+  console.log(coverPic);
+
   return (
     <>
-      <div className="bg-gray-300">
-      <Head>
-        <title>Profile Page</title>
-        <meta name="description" content="Profile Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header value={inputValue} onChange={setInputValue} />
+      <div className="bg-gray-200">
+        <Head>
+          <title>Profile Page</title>
+          <meta name="description" content="Profile Page" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header value={inputValue} onChange={setInputValue} />
 
-      <div className="text-white w-auto flex flex-col justify-start">
-        <div className="flex flex-col">
-
-          <div>
-            <div>
-                <div className="h-80">
-                  <Image 
-                      className="border-8 border-lime-600 rounded-md" 
-                      src={coverPic} 
-                      width={1600} 
-                      height={450} 
-                      alt={data.profileDetails[0].name} />
-                </div>
-                <div>
-                  <div className="flex">
-                    <Image 
-                      className="border-8 border-lime-600 rounded-md" 
-                      src={profilePic} 
-                      width={200} 
-                      height={200} 
-                      alt={data.profileDetails[0].name} />
-                  </div>
-                  <div>
-                    {data.profileDetails[0].name}
-                  </div>
-                  <div>
-                    @{data.profileDetails[0].handle}
-                  </div>                  
-                </div>
+        <div className="text-white flex flex-col justify-start">
+          <div className="flex flex-col">
+            <div className="h-80">
+              <Image
+                className="rounded-md"
+                src={coverPic}
+                width={1600}
+                height={450}
+                alt={data.profileDetails[0].name}
+              />
             </div>
-
-
-            <div className="grid grid-cols-3">
-                <div className="cols-span-1">
-                  {data.profileDetails[0].bio}
-                </div>
-                <div className="cols-span-2">
-                  <div>
-                    {data.profileDetails[0].stats.totalPublications} Publications
-                  </div>
-                  <div>
-                    {data.profileDetails[0].stats.totalCollects} Collects
-                  </div>
-                  <div>
-                    {data.profileDetails[0].stats.totalFollowers} Followers
-                  </div>
-                  <div>
-                    {data.profileDetails[0].stats.totalFollowing} Following
-                  </div>
-                </div>
+            <div className="flex justify-center">
+              <Image
+                className="rounded-full"
+                src={profilePic}
+                width={200}
+                height={200}
+                alt={data.profileDetails[0].name}
+              />
             </div>
-
-            <div>
-                Mutual Followers
-                <MutualFollowers viewingProfileId={viewingProfileId} yourProfileId={yourProfileId} />
-                <div>
-
-                </div>
+            <div className="flex flex-col items-center mt-4 font-gotham">
+              <div className="text-emerald-800 text-2xl font-bold">
+                {data.profileDetails[0].name}
               </div>
-
+              <div className="text-gray-400 text-xl">
+                @{data.profileDetails[0].handle}
+              </div>
+              <div className="text-emerald-900 text-xl mt-2 font-semibold font-gotham">
+                {data.profileDetails[0].bio}
+              </div>
+            </div>
           </div>
 
+          <div className="grid grid-cols-2 gap-4 container mx-auto text-emerald-900">
+            <div className="col-span-1 flex flex-row-reverse">
+              <div>
+                <span className="font-bold">
+                  {" "}
+                  {data.profileDetails[0].stats.totalPublications}
+                </span>{" "}
+                Publications
+              </div>
+            </div>
+            <div className=" col-span-1">
+              <div>
+                <span className="font-bold">
+                  {data.profileDetails[0].stats.totalCollects}
+                </span>{" "}
+                Collects
+              </div>
+            </div>
+            <div className="col-span-1 flex flex-row-reverse">
+              <div>
+                <span className="font-bold">
+                  {data.profileDetails[0].stats.totalFollowers}
+                </span>{" "}
+                Followers
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div>
+                <span className="font-bold">
+                  {data.profileDetails[0].stats.totalFollowing}
+                </span>{" "}
+                Following
+              </div>
+            </div>
+          </div>
         </div>
+
+        <div>
+          Mutual Followers
+          <MutualFollowers
+            viewingProfileId={viewingProfileId}
+            yourProfileId={yourProfileId}
+          />
+          <div></div>
+        </div>
+
+        <ProfilePublications profileId={viewingProfileId} />
       </div>
-
-      <ProfilePublications viewingProfileId={viewingProfileId} />
-
-    </div>
     </>
   );
 };
