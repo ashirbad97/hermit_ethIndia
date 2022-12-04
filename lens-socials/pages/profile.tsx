5a3e6c3c4ed00956708389e2754141d2ac966cb6
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-04 05:32:44 +0530

    built the profile page

--- lens-socials/pages/profile.tsx
+++ lens-socials/pages/profile.tsx
@@ -4,6 +4,8 @@ import Header from "../components/Header";
 import useSWR from "swr";
 import Image from "next/image";
 
+import MutualFollowers from "../components/MutualFollowers";
+
 import { Profile } from "../types/profile";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
@@ -12,31 +14,41 @@ const UserProfile: FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
   
   // fetch and set profile handle here
-  const profileHandle = "millionrecords.lens";
-  const { data, error } = useSWR(
-    `/api/fetchProfileDetailsByHandle?handles=${profileHandle}`,
+  const viewingpProfileHandle = "yoginth.test";
+
+
+
+  //your profile handle will be passed from parent
+  const yourProfileId = "ashirbad97.test";
+
+  const { data , error } = useSWR(
+    `/api/fetchProfileDetailsByHandle?handles=${viewingpProfileHandle}`,
     fetcher
   );
   if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>; 
-  console.log(data);
+  if (!data) return <div>loading...</div>;
+  
+
+  // fetching the profileID from fetched data to get the mutual followers
+  const viewingProfileId: string = data?.profileDetails[0].id;
 
   let profilePic: string = "";
-  if (data?.profile?.picture?.original?.url != undefined) {
-    profilePic = (data.profile.picture?.original?.url).includes("ipfs://")
-      ? "https://ipfs.io/" + data.profile.picture?.original?.url.replace(":/", "")
-      : data.profile.picture?.original?.url;
+  if (data?.profileDetails[0]?.picture?.original?.url != undefined) {
+    profilePic = (data.profileDetails[0].picture?.original?.url).includes("ipfs://")
+      ? "https://ipfs.io/" + data.profileDetails[0].picture?.original?.url.replace(":/", "")
+      : data.profileDetails[0].picture?.original?.url;
   }
 
   let coverPic: string = "";
-  if (data?.profile?.coverPicture?.original?.url != undefined) {
-    coverPic = (data.profile.pcoverPictureicture?.original?.url).includes("ipfs://")
-      ? "https://ipfs.io/" + data.profile.coverPicture?.original?.url.replace(":/", "")
-      : data.profile.coverPicture?.original?.url;
+  if (data?.profileDetails[0]?.coverPicture?.original?.url != undefined) {
+    coverPic = (data.profileDetails[0].coverPicture?.original?.url).includes("ipfs://")
+      ? "https://ipfs.io/" + data.profileDetails[0].coverPicture?.original?.url.replace(":/", "")
+      : data.profileDetails[0].coverPicture?.original?.url;
+  } else {
+    coverPic = "https://picsum.photos/1600/450";
   }
-
+console.log(coverPic);
   
-
   return (
     <>
       <div className="bg-gray-300">
@@ -49,49 +61,66 @@ const UserProfile: FC = () => {
 
       <div className="text-white w-auto flex flex-col justify-start">
         <div className="flex flex-col">
-          {data.profileDetails.map((profile: Profile) => (
-            <>
-              <div>
+
+          <div>
+            <div>
                 <div className="h-80">
-                  {coverPic}
+                  <Image 
+                      className="border-8 border-lime-600 rounded-md" 
+                      src={coverPic} 
+                      width={1600} 
+                      height={450} 
+                      alt={data.profileDetails[0].name} />
                 </div>
                 <div>
-                  <div>
-                    <Image src={profilePic} width={32} height={32} alt={profile.name} />
+                  <div className="flex">
+                    <Image 
+                      className="border-8 border-lime-600 rounded-md" 
+                      src={profilePic} 
+                      width={200} 
+                      height={200} 
+                      alt={data.profileDetails[0].name} />
                   </div>
                   <div>
-                    {profile.name}
+                    {data.profileDetails[0].name}
                   </div>
                   <div>
-                    @{profile.handle}
+                    @{data.profileDetails[0].handle}
                   </div>                  
                 </div>
-              </div>
-              <div className="grid grid-cols-3">
+            </div>
+
+
+            <div className="grid grid-cols-3">
                 <div className="cols-span-1">
-                  {profile.bio}
+                  {data.profileDetails[0].bio}
                 </div>
                 <div className="cols-span-2">
                   <div>
-                    {profile.stats.totalPublications} Publications
+                    {data.profileDetails[0].stats.totalPublications} Publications
                   </div>
                   <div>
-                    {profile.stats.totalCollects} Collects
+                    {data.profileDetails[0].stats.totalCollects} Collects
                   </div>
                   <div>
-                    {profile.stats.totalFollowers} Followers
+                    {data.profileDetails[0].stats.totalFollowers} Followers
                   </div>
                   <div>
-                    {profile.stats.totalFollowing} Following
+                    {data.profileDetails[0].stats.totalFollowing} Following
                   </div>
+                </div>
+            </div>
 
-                  
+            <div>
+                Mutual Followers
+                <MutualFollowers viewingProfileId={viewingProfileId} yourProfileId={yourProfileId} />
+                <div>
+
                 </div>
               </div>
 
+          </div>
 
-            </>
-          ))}
         </div>
       </div>
 
